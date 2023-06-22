efff8e44173ecf1bd737f93f4a67191f8860252d
David Cox <STUDENTS\david.ryan.cox88@a-t07x59w5mpes.ad1.students.ga.co>
2023-06-22 15:58:24 +0100

    Thurs 22nd June updates to API

--- config/router.js
+++ config/router.js
@@ -1,13 +1,15 @@
-import express from 'express';
+import express from "express";
+import moviesController from "../controllers/moviesController.js";
 
 const Router = express.Router();
 
-Router.route('/movies')
-  .get(function (req, res) {
-    res.send('request to GET all movies');
-  })
-  .post(function (req, res) {
-    res.send('request to CREATE new movie');
-  });
+Router.route("/movies")
+  .get(moviesController.getAllMovies)
+  .post(moviesController.createMovie);
+
+Router.route("/movies/:id")
+  .get(moviesController.getMovie)
+  .put(moviesController.updateMovie)
+  .delete(moviesController.deleteMovie);
 
 export default Router;
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-import express from 'express';
-import router from './config/router.js';
+import express from "express";
+import router from "./config/router.js";
+import db from "./db/helpers.js";
 
 const app = express();
 
 app.use(router);
 
-// change this port variable to be an environment variable
-app.listen(3000, function () {
-  console.log('App is listening on port 3000');
-});
+async function startServer() {
+  try {
+    await db.connect();
+    console.log("Mongoose is connected to the database");
+    // change this port variable to be an environment variable
+    app.listen(3000, function () {
+      console.log("App is listening on port 3000");
+    });
+  } catch (err) {
+    console.log("Something when wrong connecting to the database");
+  }
+}
+
+startServer();
